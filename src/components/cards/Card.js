cbe8c41069ee1fb088406811e9dc5d2d6cf5376e
cepeda <carlos@cepeda.me>
2018-02-26 12:09:39 +0100

    Less card shadow

--- src/components/cards/Card.js
+++ src/components/cards/Card.js
@@ -34,7 +34,7 @@ const styles = StyleSheet.create({
     borderRadius: BORDER_RADIUS,
     shadowOffset: { width: 3, height: 3 },
     shadowColor: "black",
-    shadowOpacity: 0.7,
+    shadowOpacity: 0.35,
     shadowRadius: 3
   },
   body: {

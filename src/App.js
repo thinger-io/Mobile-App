280ea79155155c0efc0a3e81c46ba181ed0a6957
cepeda <carlos@cepeda.me>
2018-03-15 08:43:35 +0100

    Orientation listener on entry point

--- src/App.js
+++ src/App.js
@@ -5,17 +5,29 @@
  */
 
 import React, { Component } from "react";
-import { StatusBar } from "react-native";
+import { StatusBar, Dimensions } from "react-native";
 import { Provider } from "react-redux";
 import configureStore from "./configs/Store";
 import { PersistGate } from "redux-persist/es/integration/react";
 import Navigator from "./components/navigation/Navigator";
+import {setOrientation} from "./actions/orientation";
 
 const { store, persistor } = configureStore();
 
 type Props = {};
 export default class App extends Component<Props> {
+
+  onChangeOrientation = (event: any) => {
+    const { width, height } = event.window;
+    const orientation = width > height ? "LANDSCAPE" : "PORTRAIT";
+    if (store.orientation !== orientation)
+      store.dispatch(setOrientation(orientation));
+    };
+
   render() {
+    this.onChangeOrientation({ window: Dimensions.get("window") });
+    Dimensions.addEventListener("change", this.onChangeOrientation);
+
     return (
       <Provider store={store}>
         <PersistGate persistor={persistor}>
